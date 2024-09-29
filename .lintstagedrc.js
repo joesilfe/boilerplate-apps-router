03d5348dfbf3a7ae6d486511a3a3b4abc724e8fb
Joel Silva <joel.silva.ferr@gmail.com>
2024-09-29 15:48:38 -0300

    teste break five

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,7 +2,7 @@ module.exports = {
   // Lint & Prettify TS and JS files
   '*.{js, jsx, ts, tsx}': (filenames) => [
     `npx prettier src/ --write ${filenames.join(' ')}`,
-    `npm run lint --fix . ${filenames.join(' --file')}`,
+    `npm run lint --allow-empty --fix . ${filenames.join(' --file')}`,
     `npm run test -- --findRelatedTests ${filenames.join(' ')}`
   ]
 }
