1112b0c54da38d298cda200add60f6ef8f5a09ad
Joel Silva <joel.silva.ferr@gmail.com>
2024-09-29 16:44:36 -0300

    add styled components

--- jest.config.ts
+++ jest.config.ts
@@ -1,18 +1,22 @@
 import type { Config } from 'jest'
 import nextJest from 'next/jest.js'
- 
+
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
   dir: './',
 })
- 
+
 // Add any custom config to be passed to Jest
 const config: Config = {
   coverageProvider: 'v8',
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/stories.tsx'],
+  collectCoverageFrom: [
+      'src/**/*.ts(x)?', 
+      '!src/app/**', // shoud be test in e2e
+      '!src/lib/registry.tsx'
+    ],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
   modulePaths: ['<rootDir>/src/'],
   transform: {
@@ -23,6 +27,6 @@ const config: Config = {
   // Add more setup options before each test is run
   // setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 }
- 
+
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
 export default createJestConfig(config)
