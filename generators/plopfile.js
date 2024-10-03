9b7c1444947f1441108653a3cd36b6b84c979c0c
Joel Silva <joel.silva.ferr@gmail.com>
2024-10-03 01:20:47 -0300

    add function plop

---
+++ generators/plopfile.js
@@ -0,0 +1,40 @@
+// Types provided from the official repo:
+// https://github.com/styled-components/jest-styled-components/blob/master/typings/index.d.ts
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/ban-types */
+module.exports = (plop) => {
+  // create your generators here
+  plop.setGenerator('components', {
+    description: 'Create a component',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is your component name?'
+      }
+    ], // array of inquirer prompts
+    actions: [
+      {
+        type: 'add',
+        path: '../src/components/{{pascalCase name}}/index.tsx',
+        templateFile: 'templates/Component.tsx.hbs'
+      },
+      {
+        type: 'add',
+        path: '../src/components/{{pascalCase name}}/styles.ts',
+        templateFile: 'templates/styles.ts.hbs'
+      },
+      {
+        type: 'add',
+        path: '../src/components/{{pascalCase name}}/stories.tsx',
+        templateFile: 'templates/stories.tsx.hbs'
+      },
+      {
+        type: 'add',
+        path: '../src/components/{{pascalCase name}}/test.tsx',
+        templateFile: 'templates/test.tsx.hbs'
+      }
+    ]
+  })
+}
